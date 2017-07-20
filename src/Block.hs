22b848a45f56218d7036a9f09ceb4ddf1b3889af
Bibek Pandey <bibek@janakitech.com>
2017-07-20 17:50:04 +0545

    update

---
+++ src/Block.hs
@@ -0,0 +1,33 @@
+module Block where
+
+import Crypto.Hash
+import Crypto.Hash.Algorithms
+import Data.ByteString.UTF8 as B
+
+data Block = Block {
+      index :: Int
+    , blockData :: String -- this might change
+    , blockHash :: String
+    , previousBlockHash :: String
+    , timestamp :: String -- this will change
+}
+
+-- calculation of hash from index, prevhash, timestamp and hash data
+calculateHash :: Int -> String -> String -> String -> IO String
+calculateHash index prevHash timestamp hashData = do
+    bytearr <- fmap B.fromString (return plain)
+    let hsh = hash bytearr:: Digest SHA512
+    return $ show hsh
+    where plain = (show index) ++ prevHash ++ timestamp ++ hashData
+
+
+generateNewBlock :: Block -> String -> IO Block
+generateNewBlock block newdata = do
+    newhash <- calculateHash newind prevhash timestamp newdata
+    return $ Block {
+          index = newind
+        , blockData = newdata
+        , blockHash = newhash
+        , previousBlockHash = prevhash
+        , timestamp = timestamp -- TODO: update timestamp
+    }
